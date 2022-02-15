137a73bd1c6ebb82f1a59d7a9a097ddca8ec83c5
GeisslerJ <99483698+GeisslerJ@users.noreply.github.com>
2022-02-15 16:16:20 +0100

    Update main.tf
    ..

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "docker" {
-  host = "ssh://docker@192.168.57.100:22"
+  host = "ssh://docker@192.168.57.100"
 }
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
